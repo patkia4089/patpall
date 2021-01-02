5c00c53543bf576f910cd1f451785f5aff8c37af
patkia4089 <76836779+patkia4089@users.noreply.github.com>
2021-01-02 12:08:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,21 @@
 
 include_once("index.html");
 
+$to = "patpal4089@gmail.com";
+$subject = "email from my website:$sebject";
+
+
+
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$email = $_POST['email'];
+$comment = $_POST['comment'];
+
+
+$headers .= "content-type:text/html;\r\n";
+$headers .= "from:$email";
+
+
+mail($to,$subject,$comment,$headers);
+echo "thankyou,your email has been sent."
 ?>
